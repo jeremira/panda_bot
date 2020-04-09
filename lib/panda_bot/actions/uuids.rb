66254137b7cd9ca89320b620d9ebce709a82d0d0
jeremie <jeremie.miraille@hivency.com>
2020-04-09 13:49:23 +0200

    add spec for tag uuid call

---
+++ lib/panda_bot/actions/uuids.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+module PandaBot
+  module Actions
+    class Uuids
+      attr_reader :agent, :uuid
+
+      def initialize
+        @agent = PandaBot::Agent.new
+        @uuid = agent.find_task(gid: '1168601077028969').notes
+
+        raise StandardError, "Invalid Uuid reference from Asana : #{uuid}" unless /^[A-Z]{3}-\d*$/.match?(uuid.to_s)
+      end
+
+      def tag!
+        agent.sprint_tasks.each { |task| add_uuid task }
+        agent.backlog_tasks.each { |task| add_uuid task }
+        agent.find_task(gid: '1168601077028969').update(notes: ref_uuid)
+      end
+      # 
+      # def hivency_tasks
+      #   SPRINT_PROJECT_GID = '1139348995392150' # sprint project gid
+      #   BACKLOG_PROJECT_GID = '1139062746771721' #
+      #   client.tasks.find_all(project: SPRINT_PROJECT_GID)
+      #   client.tasks.find_all(project: BACKLOG_PROJECT_GID)
+      # end
+
+      def add_uuid(task)
+        return unless task.is_a? Asana::Resources::Task
+        return unless task.custom_fields.find { |f| f['gid'] == '1168611057004190' && f['text_value'].to_s.strip.empty? }
+
+        @uuid = @uuid.gsub(/\d+/) do |match|
+          match.to_i + 1
+        end
+        task.update({ custom_fields: { '1168611057004190' => @uuid } })
+
+        uuid
+      end
+    end
+  end
+end
+#
+# task.subtasks.each do |task|
+#   puts "updating sub task : #{task.name}"
+#   task = client.tasks.find_by_id(task.gid)
+#
+#   next if pas Asana task
+#   next unless task.custom_fields.find { |f| f['gid'] == '1168611057004190' && f['text_value'].to_s.strip.empty? }
+#
+#   ref_uuid = ref_uuid.gsub(/\d+/) do |match|
+#     match.to_i + 1
+#   end
+#   task.update({ custom_fields: { uuid_field_id => ref_uuid } })
+# end
+#   uuid_task.update(notes: ref_uuid)
---
+++ spec/panda_bot/action/uuids_spec.rb
@@ -0,0 +1,149 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe PandaBot::Actions::Uuids do
+  let(:tagger) { described_class.new }
+  let(:agent_double) { instance_double PandaBot::Agent }
+  let(:ref_task_double) { instance_double Asana::Resources::Task, notes: 'MOK-123', update: true }
+
+  before do
+    allow(PandaBot::Agent).to receive(:new).and_return(agent_double)
+    allow(agent_double).to receive(:find_task).and_return(ref_task_double)
+  end
+
+  describe 'Initialize' do
+    context 'with available Uuid reference' do
+      it 'instanciates itself' do
+        expect(tagger).to be_a described_class
+      end
+
+      it 'exposes his agent' do
+        expect(tagger.agent).to eq agent_double
+      end
+
+      it 'queries task datas from agent' do
+        tagger
+        expect(agent_double).to have_received(:find_task).once.with(gid: '1168601077028969')
+      end
+
+      it 'expose reference task details' do
+        expect(tagger.uuid).to eq 'MOK-123'
+      end
+    end
+
+    context 'without available Uuid reference' do
+      let(:ref_task_double) { OpenStruct.new gid: '1168601077028969' }
+
+      it 'raises no reference error' do
+        expect { tagger }.to raise_error StandardError, 'Invalid Uuid reference from Asana : '
+      end
+    end
+
+    context 'with invalid Uuid reference' do
+      let(:ref_task_double) { instance_double Asana::Resources::Task, notes: 'KABOOM' }
+
+      it 'raises no reference error' do
+        expect { tagger }.to raise_error StandardError, 'Invalid Uuid reference from Asana : KABOOM'
+      end
+    end
+  end
+
+  describe 'tag!' do
+    let(:tested_method) { tagger.tag! }
+    let(:task1) { instance_double Asana::Resources::Task }
+    let(:task2) { instance_double Asana::Resources::Task }
+    let(:task3) { instance_double Asana::Resources::Task }
+
+    before do
+      allow(agent_double).to receive(:sprint_tasks).and_return(sprint_tasks_dbl)
+      allow(agent_double).to receive(:backlog_tasks).and_return(backlog_tasks_dbl)
+      allow(tagger).to receive(:add_uuid)
+    end
+
+    context 'with some tasks' do
+      let(:sprint_tasks_dbl) { [task1, task2] }
+      let(:backlog_tasks_dbl) { [task3] }
+
+      it 'tag task1 uuid' do
+        tested_method
+        expect(tagger).to have_received(:add_uuid).once.with(task1)
+      end
+
+      it 'tag task2 uuid' do
+        tested_method
+        expect(tagger).to have_received(:add_uuid).once.with(task2)
+      end
+
+      it 'tag task3 uuid' do
+        tested_method
+        expect(tagger).to have_received(:add_uuid).once.with(task3)
+      end
+
+      it 'update uuid reference in database' do
+        tested_method
+        expect(ref_task_double).to have_received(:update).once.with(notes: 'MOK-123')
+      end
+    end
+
+    context 'without any tasks' do
+      let(:sprint_tasks_dbl) { [] }
+      let(:backlog_tasks_dbl) { [] }
+
+      it 'does not add any uuid' do
+        tested_method
+        expect(tagger).not_to have_received(:add_uuid)
+      end
+
+      it 'does not update uuid reference in database' do
+        tested_method
+        expect(ref_task_double).not_to have_received(:update)
+      end
+    end
+  end
+
+  describe 'add_uuid' do
+    let(:tested_method) { tagger.add_uuid task }
+    let(:task) do
+      instance_double Asana::Resources::Task, is_a?: true, update: true,
+                                              custom_fields: [{ 'gid' => '1168611057004190', 'text_value' => nil }]
+    end
+
+    context 'with a valid task' do
+      it 'updates the task' do
+        tested_method
+        expect(task).to have_received(:update).once.with(custom_fields: { '1168611057004190' => 'MOK-124' })
+      end
+
+      it 'increments the uuid' do
+        tested_method
+        expect(tagger.uuid).to eq 'MOK-124'
+      end
+
+      it 'returns the uuid' do
+        expect(tested_method).to eq 'MOK-124'
+      end
+    end
+
+    context 'without a valid task' do
+      let(:task) do
+        instance_double Asana::Resources::Task, is_a?: false, update: true,
+                                                custom_fields: [{ 'gid' => '1168611057004190', 'text_value' => nil }]
+      end
+
+      it 'does not update the task' do
+        tested_method
+        expect(task).not_to have_received(:update)
+      end
+
+      it 'does not increment the uuid' do
+        tested_method
+        expect(tagger.uuid).to eq 'MOK-123'
+      end
+
+      it 'returns nil' do
+        expect(tested_method).to eq nil
+      end
+    end
+  end
+end
